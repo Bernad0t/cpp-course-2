07cc94faa5c4efc50606bb7ef9376437101304b5
Bernad0t <dnsvesna27092015@gmail.com>
2024-10-15 18:45:36 +0300

    before change unar binar

--- calc lab1 sem5/calculator.cpp
+++ calc lab1 sem5/calculator.cpp
@@ -66,7 +66,7 @@ void Calculator::ProcessString(string str) {
 					bracketVisited = true;
 			}
 			if (!bracketVisited)
-				throw string("open bracket don't exist");
+				throw string("one bracket don't exist");
 			operStack.pop();
 			i++;
 		}
